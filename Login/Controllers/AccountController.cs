c609f927a0b537940010b8a2dd5787990e93d7a7
Promise Tsoeu <promisetsoeu@gmail.com>
2021-11-19 18:01:17 +0200

    User login

---
+++ Login/Controllers/AccountController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Login_MVC_.Models;
+using System.Data.SqlClient;
+
+namespace Login_MVC_.Controllers
+{
+    public class AccountController : Controller
+    {
+        SqlConnection con = new SqlConnection();
+        SqlCommand com = new SqlCommand();
+        SqlDataReader dr;
+        // GET: Account
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        void connectionString()
+        {
+            con.ConnectionString = "data source=projects2.database.windows.net; database=logging-in; intergrated security = SSPI;";        
+        }
+        public ActionResult Verify(Account accnt)
+        {
+            connectionString();
+            con.Open();
+            com.Connection = con;
+            com.CommandText = "select * from tbl_login where username'"+accnt.Name+"'and password'"+accnt.Password+"'";
+            dr = com.ExecuteReader();
+            if (dr.Read())
+            {
+                con.Close();
+                return View();
+            }
+            else
+            {
+                con.Close();
+                return View();
+            }
+            
+            
+        }
+    }
+}
